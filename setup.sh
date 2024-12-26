4c58a9c2cfc1cb59a27d124d5ccaba6c66165932
RajuBuddharaju <97377166+RajuBuddharaju@users.noreply.github.com>
2024-12-26 13:20:27 +0100

    More font stuff.

--- setup.sh
+++ setup.sh
@@ -5,6 +5,10 @@ ln -sfn ~/dotfiles/nvim ~/.config/nvim
 ln -sfn ~/dotfiles/i3 ~/.config/i3
 ln -sfn ~/dotfiles/bashrc ~/.bashrc
 ln -sfn ~/dotfiles/zshrc ~/.zshrc
-ln -sfn ~/dotfiles/fonts ~/.local/share/fonts
 ln -sfn ~/dotfiles/kitty ~/.config/kitty
 
+# Ensure ~/.local/share/fonts exists
+mkdir -p ~/.local/share/fonts
+
+# Symlink CONTENTS of ~/dotfiles/fonts (Not the Folder)
+ln -sfn ~/dotfiles/fonts/* ~/.local/share/fonts/
