96d68b4b4a6d05f14959b6a174cb82bfa1885e0a
Narasihma Raju Buddharaju <97377166+RajuBuddharaju@users.noreply.github.com>
2024-12-26 13:23:01 +0100

    Destructive fonts linking.

--- setup.sh
+++ setup.sh
@@ -7,8 +7,6 @@ ln -sfn ~/dotfiles/bashrc ~/.bashrc
 ln -sfn ~/dotfiles/zshrc ~/.zshrc
 ln -sfn ~/dotfiles/kitty ~/.config/kitty
 
-# Ensure ~/.local/share/fonts exists
-mkdir -p ~/.local/share/fonts
+rm -rf ~/.local/share/fonts
+ln -s ~/dotfiles/fonts ~/.local/share/fonts
 
-# Symlink CONTENTS of ~/dotfiles/fonts (Not the Folder)
-ln -sfn ~/dotfiles/fonts/* ~/.local/share/fonts/
