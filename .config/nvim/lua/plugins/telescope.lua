0d79478e1aeaccf09a8890cf925dbc99e1a775bd
RajuBuddharaju <97377166+RajuBuddharaju@users.noreply.github.com>
2025-10-04 18:12:27 +0200

    Added scroll to tmux and hidden files on telescope.

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,5 +1,34 @@
+-- plugin spec (lazy.nvim or similar)
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
--- or                              , branch = '0.1.x',
-      dependencies = { 'nvim-lua/plenary.nvim' }
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.8',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    require('telescope').setup{
+      -- Make ripgrep include hidden files, but ignore .git
+      defaults = {
+        vimgrep_arguments = {
+          'rg',
+          '--color=never',
+          '--no-heading',
+          '--with-filename',
+          '--line-number',
+          '--column',
+          '--smart-case',
+          '--hidden',
+          '--glob', '!**/.git/*',
+        },
+      },
+      pickers = {
+        -- Show hidden files in the file picker by default
+        find_files = {
+          hidden = true,
+          -- Use ripgrep for listing files, include hidden, skip .git
+          find_command = { 'rg', '--files', '--hidden', '--glob', '!**/.git/*' },
+          follow = true,
+        },
+      },
     }
+  end,
+}
+
